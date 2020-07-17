b04325d630d643210076d839da22cd6adb7d9f5f
unknown <jethanhargrave@gmail.com>
2020-07-17 13:20:09 -0400

    mvp

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -21,14 +21,54 @@ room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
-world.print_rooms()
+# world.print_rooms()
 
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+dictionary = {}
 
+def traversal(room, visited=None):
+    exits = room.get_exits() 
+    if room.id in dictionary: #base case
+        if len(exits) == len(dictionary[room.id]): 
+            if room.id in visited:
+                return
+    if visited == None: #create set
+        visited = set()
+    visited.add(room.id) #add room to set
+    if room.id not in dictionary: 
+        dictionary[room.id] = {}
+    for ext in exits: 
+        if player.current_room.get_room_in_direction(ext).id not in visited:
+            if ext not in dictionary[room.id]:
+                player.travel(ext)
+                traversal_path.append(ext)
+                if len(player.current_room.get_exits()) == 1:
+                    visited.add(player.current_room.id)
+            if player.current_room.id not in visited:
+                traversal(player.current_room, visited)
+            direction = ""
+            if ext == "n":
+                direction = "s"
+            if ext == "s":
+                direction = "n"
+            if ext == "w":
+                direction = "e"
+            if ext == "e":
+                direction = "w"
+            if player.current_room.id not in dictionary:
+                dictionary[player.current_room.id] = {}
+            if direction not in dictionary[player.current_room.id]:
+                dictionary[player.current_room.id][direction] = room.id
+            player.travel(direction)
+            traversal_path.append(direction)
+
+player.current_room = world.starting_room
+
+traversal(player.current_room)
 
 
 # TRAVERSAL TEST
